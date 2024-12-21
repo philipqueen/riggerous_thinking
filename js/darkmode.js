0b307e725e58581cfdd13ff3fc3cc28ddea448a1
philipqueen <queen.philip@gmail.com>
2024-12-20 23:35:20 -0700

    fix link behavior, try fixing darkmode toggle out of synch

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,8 @@
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
 if (isDarkMode) {
     document.documentElement.classList.add('dark-mode');
+    toggleIcon.classList.remove('light-mode');
+} else {
+    toggleIcon.classList.add('light-mode');
+    toggleIcon.classList.remove('dark-mode');
 }
